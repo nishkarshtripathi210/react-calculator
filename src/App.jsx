420e1bc28b1781ef5f7e2f4271e2159f02eb6c41
Nishkarsh Tripathi <nishkarshtripathi210@gmail.com>
2026-09-25 04:32:05 +0530

    Make it functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
 import styles from './App.module.css';
 import Display from './components/Display';
 import ButtonsContainer from './components/ButtonsContainer';
+import {useState} from "react";
 
 function App() {
+
+  let [calVal, setCalVal] = useState("");
+  const onButtonClick = (buttonText) => 
+    { 
+      if(buttonText === 'C'){
+
+        setCalVal("");
+
+      }else if(buttonText === '=') {
+
+        const result= eval(calVal);
+        setCalVal(result);
+
+
+    } else {
+      const newDisplayValue = calVal + buttonText;
+      setCalVal(newDisplayValue);
+    }
+  }
+
+
+
   return (
     <div className ={styles.calculator}>
-      <Display/>
-      <ButtonsContainer/>
+      <Display displayValue = {calVal}/>
+      <ButtonsContainer onButtonClick={onButtonClick}/>
       
     </div>
   );
--- src/components/ButtonsContainer.jsx
+++ src/components/ButtonsContainer.jsx
@@ -1,16 +1,32 @@
-import styles from './ButtonsContainer.module.css';
-const ButtonContainer = () =>{
+import styles from "./ButtonsContainer.module.css";
+const ButtonContainer = ({onButtonClick}) => {
+  const buttonNames = [
+    "C",
+    "1",
+    "2",
+    "+",
+    "3",
+    "4",
+    "-",
+    "5",
+    "6",
+    "*",
+    "7",
+    "8",
+    "/",
+    "=",
+    "9",
+    "0",
+    ".",
+  ];
 
-  const buttonName = ['C', '1', '2', '+', '3', '4', '-', '5', '6', '*', '7', '8', '/', '=', '9', '0', '.'];
- 
- 
- return <div className = {styles.buttonContainer}>
+  return (
+    <div className={styles.buttonContainer}>
+      {buttonNames.map((buttonName) => (
+        <button className={styles.button} onClick={() => onButtonClick(buttonName)}> {buttonName}</button>
+      ))}
+    </div>
+  );
+};
 
-        {buttonName.map(buttonName => <button className = {styles.button}> {buttonName}</button>)}
-
-
-      </div>
-
- };
-
- export default ButtonContainer;
+export default ButtonContainer;
